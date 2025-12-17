05a682a369649a5c5fe748a9d02ce2d0220138cf
monika <gopal21@gmail.com>
2025-12-17 22:03:28 +0530

    completed

--- props and state management/q.2/q.2/src/Calculator.jsx
+++ props and state management/q.2/q.2/src/Calculator.jsx
@@ -1,23 +1,65 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 function Calculator() {
+  const [num1, setNum1] = useState("");
+  const [num2, setNum2] = useState("");
+  const [operation, setOperation] = useState("add");
+  const [results, setResults] = useState([]);
+
+  const handleCalculate = () => {
+    let result;
+
+    const a = Number(num1);
+    const b = Number(num2);
+
+    if (operation === "add") {
+      result = a + b;
+    } else if (operation === "subtract") {
+      result = a - b;
+    } else if (operation === "multiply") {
+      result = a * b;
+    }
+
+    setResults([...results, result]);
+  };
+
   return (
     <div>
-      <form>
-          <input type="numbers" placeholder='Enter numbers'/>
-          <input type="numbers" placeholder='Enter numbers'/>
-         
-          <br/><br />
-          <select id="selectOperations">
-            <option value="Add">Add</option>
-            <option value="Subtract">Subtract</option>
-            <option value="Multiply">Multiply</option>
-          </select>
-          <label htmlFor=""></label>
-          <button>Click</button>
-      </form>
+      <h2>Basic Calculator</h2>
+      <br />
+
+      <input
+        type="number"
+        value={num1}
+        onChange={(e) => setNum1(e.target.value)}
+        placeholder="First number"/>
+
+      <input
+        type="number"
+        value={num2}
+        onChange={(e) => setNum2(e.target.value)}
+        placeholder="Second number"/>
+
+      <select
+        value={operation}
+        onChange={(e) => setOperation(e.target.value)} >
+        <option value="add">Add</option>
+        <option value="subtract">Subtract</option>
+        <option value="multiply">Multiply</option>
+      </select>
+
+      <button onClick={handleCalculate}>Perform Action</button>
+      <br/>
+      <br/>
+      <h3>Results:</h3>
+      <ul>
+        {results.map((res, index) => (
+          <li key={index}>{res}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Calculator
+export default Calculator;
